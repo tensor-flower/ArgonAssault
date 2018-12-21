6b81060b701d1aa46667925eebe34beb3b0ba85a
tensor-flower <wanqiguo2@gmail.com>
2018-12-21 18:49:53 +0800

    baseline working

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,16 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿//using System.Collections;
+//using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+    //config params
     [SerializeField] GameObject explosionFX;
+    [SerializeField] int destroyScore;
+
+    //cache
     BoxCollider myCollider;
+    bool isHit = false;
+    ScoreBoard scoreBoard;
 
     private void Start()
     {
         myCollider = gameObject.AddComponent<BoxCollider>();
         myCollider.isTrigger = false;
+        scoreBoard = FindObjectOfType<ScoreBoard>();
     }
 
     private void OnParticleCollision(GameObject other)
@@ -18,7 +25,14 @@ public class Enemy : MonoBehaviour {
         Debug.Log(gameObject.name + " collided with " + other.gameObject.name);
         //explosionFX = transform.Find("Explosion").gameObject;
         //explosionFX.SetActive(true);
-        Instantiate(explosionFX, transform.position, Quaternion.identity);
-        Destroy(gameObject);
+        if(!isHit)
+        {
+            isHit = true;         
+            scoreBoard.AddScore(destroyScore);  
+            GameObject explosion = Instantiate(explosionFX, transform.position, Quaternion.identity);
+            Destroy(explosion, 1f);
+            Destroy(gameObject);
+        }
     }
+
 }
---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,23 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour {
+	Text text;
+	int score = 0;
+	
+	void Start () {
+		text = GetComponent<Text>();
+		Display();
+	}
+
+	private void Display(){
+		text.text = score.ToString();
+	}
+
+	public void AddScore(int scoreToAdd){
+		score += scoreToAdd;
+		Display();
+	}
+}
